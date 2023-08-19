3f03e1962cef84a35a06ce8a60a30ff96977e48f
Admin <bhalalabrij73@gmail.com>
2023-08-19 10:44:53 +0530

    Trie Data Structure Inserting and Searching...

--- src/test/java/com/brij/empService/EmpServiceApplicationTests.java
+++ src/test/java/com/brij/empService/EmpServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.brij.empService;
 
+import com.brij.empService.trieSearch.DataTrieNode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmpServiceApplicationTests {
+
+	public static void main(String[] args){
+
+	}
 
 	@Test
 	void contextLoads() {
